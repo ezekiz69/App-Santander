76c822e90f13b722387b354f825ee3c8649ded92
ezequiel <ezequielsst09@gmail.com>
2021-09-15 10:11:34 -0300

    App Santander

---
+++ app/src/main/java/com/e/todolist/datasource/taskDatasource.kt
@@ -0,0 +1,24 @@
+package com.e.todolist.datasource
+
+import com.e.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list.toList()
+
+    fun insertTask(task: Task) {
+        if (task.id == 0) {
+            list.add(task.copy(id = list.size + 1))
+        } else {
+            list.remove(task)
+            list.add(task)
+        }
+    }
+
+    fun findById(taskID: Int) = list.find { it.id == taskID }
+
+    fun deleteTask(task: Task) {
+        list.remove(task)
+    }
+}
